1795a57be1f9264358ab0a9fb6718afde4b1352b
olimjonov010 <142300155+olimjonov010@users.noreply.github.com>
2024-01-19 23:57:21 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -7,3 +7,53 @@
 class Problem1 {
 public:
 };
+
+#include <iostream>
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+class Problem1 {
+public:
+    static ListNode* mergeNodes(ListNode* head) {
+        ListNode dummy(0), *tail = &dummy;
+        int sum = 0;
+        for (ListNode* node = head->next; node != nullptr; node = node->next) {
+            if (node->val == 0) {
+                if (sum > 0) {
+                    tail->next = new ListNode(sum);
+                    tail = tail->next;
+                    sum = 0;
+                }
+            } else {
+                sum += node->val;
+            }
+        }
+        return dummy.next;
+    }
+};
+ListNode* createLinkedList(const std::vector<int>& nums) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    for (int num : nums) {
+        tail->next = new ListNode(num);
+        tail = tail->next;
+    }
+    return dummy.next;
+}
+void printLinkedList(ListNode* head) {
+    for (ListNode* node = head; node != nullptr; node = node->next) {
+        std::cout << node->val << " ";
+    }
+    std::cout << std::endl;
+}
+void deleteLinkedList(ListNode* head) {
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
