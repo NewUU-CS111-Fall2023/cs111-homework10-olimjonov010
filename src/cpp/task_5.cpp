933b7c7d6e6cb0c313690a5f0f82784ff6ed5dee
olimjonov010 <142300155+olimjonov010@users.noreply.github.com>
2024-01-20 00:00:33 +0500

    Update task_5.cpp

--- src/cpp/task_5.cpp
+++ src/cpp/task_5.cpp
@@ -1,8 +1,31 @@
 /*
- * Author:
+ * Author: Odiljon Olimjonov
  * Date:
  * Name:
  */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 class Problem5 {
+public:
+    static bool hasTripletSumZero(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    return true;
+                } else if (sum < 0) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+        }
+        return false;
+    }
 };
