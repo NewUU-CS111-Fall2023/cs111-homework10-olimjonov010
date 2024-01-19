82ce1d0e21eb6adce2766e2b81b338093395f257
olimjonov010 <142300155+olimjonov010@users.noreply.github.com>
2024-01-19 23:58:17 +0500

    Update task_3.cpp

--- src/cpp/task_3.cpp
+++ src/cpp/task_3.cpp
@@ -4,5 +4,42 @@
  * Name:
  */
 
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
 class Problem3 {
+public:
+    static std::string compareNumbers(const std::string& tape) {
+        int countFirst = 0;
+        int countSecond = 0;
+        int currentState = 1; 
+        for (char symbol : tape) {
+            switch (currentState) {
+                case 1:
+                    if (symbol == '1') {
+                        countFirst++;
+                    } else {
+                        currentState = 2;
+                    }
+                    break;
+                case 2:
+                    if (symbol == '1') {
+                        countSecond++;
+                    } else {
+                        currentState = 3;
+                    }
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        if (countFirst >= countSecond) {
+            return "1";
+        } else {
+            return "11";
+        }
+    }
 };
