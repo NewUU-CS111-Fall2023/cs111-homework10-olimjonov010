f51999a8bbe7943136b4b2669c0fe9817bc3987c
olimjonov010 <142300155+olimjonov010@users.noreply.github.com>
2024-01-20 00:01:21 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -4,6 +4,23 @@
  * Name:
  */
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
 class Problem7 {
 public:
+    static int minNumberOfRabbits(std::vector<int>& answers) {
+        std::unordered_map<int, int> count;
+        int rabbits = 0;
+        for (int x : answers) {
+            if (count.find(x) == count.end() || count[x] == 0) {
+                rabbits += x + 1;
+                count[x] = x;
+            } else {
+                count[x]--;
+            }
+        }
+        return rabbits;
+    }
 };
